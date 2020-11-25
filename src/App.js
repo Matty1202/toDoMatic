fa4e9f0d32df9691776226329c333c670d80997b
Matty1202 <54592382+Matty1202@users.noreply.github.com>
2020-11-25 16:52:14 -0600

    Up to Complete a Task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import { nanoid } from "nanoid";
+import React, { useState } from "react";
 import "./styles.css";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
-export default function App(props) {
-  const taskList = props.tasks.map((task) => (
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = {
+      id: "todo-" + nanoid(),
+      name: name,
+      completed: false
+    };
+    console.log("new task: " + JSON.stringify(newTask));
+    setTasks([...tasks, newTask]);
+    console.log(tasks);
+  }
+
+  const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -11,44 +27,17 @@ export default function App(props) {
       key={task.id}
     />
   ));
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
@@ -59,3 +48,5 @@ export default function App(props) {
     </div>
   );
 }
+
+export default App;
